d2b5cf0294ee4afebd657430aae8fcaaf94cd368
ValentinoTriadi <valent122016@gmail.com>
2024-08-17 19:40:23 +0700

    fix: add package and fix module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { FilmsModule } from './api/films/films.module';
+import { AuthModule } from './api/auth/auth.module';
+import { FilmsService } from './api/films/films.service';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, FilmsModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FilmsService],
 })
 export class AppModule {}
