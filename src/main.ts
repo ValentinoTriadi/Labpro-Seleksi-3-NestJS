ad234cc8fef48f7cea2a6d252fc36e2621332237
ValentinoTriadi <valent122016@gmail.com>
2024-08-22 17:59:16 +0700

    fix: deployment

--- src/main.ts
+++ src/main.ts
@@ -57,6 +57,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+  app.listen(process.env.PORT || 3000);
 }
 bootstrap();
